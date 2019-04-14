e03c3a63374449b8882dc11a32b30bfa1ef16b15
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-13 21:13:53 -0400

    Registration endpoint creatred

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -6,7 +6,39 @@ const secrets = require("../api/secrets");
 // const Users = require("../users/users-model.js"); 
 
 router.post("/register", async (req, res) => {
+  let { name, email, password, user_type} = req.body;
 
+  if (name && email && password && user_type) {
+    const hash = bcrypt.hashSync(password, 14);
+    password = hash;
+
+    db("users")
+      .insert(req.body)
+      .then(id => {
+        db("users")
+          .where({ id })
+          .first()
+          .then(user => {
+            res.status(201).json({
+              message: `Registration successful!`,
+              user
+            });
+          })
+      })
+      .catch(error => {
+        if (error.errno === 19) {
+          res.status(403).json({
+            message: "Username already exists"
+          });
+        } else {
+          res.status(500).json(error);
+        }
+      });
+  } else {
+    res.status(400).json({
+      message: "All fields are required"
+    });
+  }
 });
 
 router.put("/login", async (req, res) => {
