cd9283069fc26b9a9274df88d46a865fd1dedc26
conconrob <connortprobertson@gmail.com>
2019-04-15 22:06:23 +0100

    test

--- data/models/usersModel.js
+++ data/models/usersModel.js
@@ -3,12 +3,14 @@ const db = require("../dbConfig");
 module.exports = { create, getUserTypeById };
 
 async function create(name, email, user_type_id, password) {
-  const id = await db("users").insert({
-    name,
-    email,
-    user_type_id,
-    password
-  });
+  const id = await db("users")
+    .insert({
+      name,
+      email,
+      user_type_id,
+      password
+    })
+    .returning("id");
   return { id, name, email };
 }
 
