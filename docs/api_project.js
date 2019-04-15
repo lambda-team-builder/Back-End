b8ce54beb0c3c5b2fb00cd2801be3df01ae76312
conconrob <connortprobertson@gmail.com>
2019-04-15 22:26:01 +0100

    docs

---
+++ docs/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "Team builder API Documentation",
+  "version": "1.0.0",
+  "description": "API team builder for Lambda School",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-04-15T21:25:41.585Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
