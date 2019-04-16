04c5794ec7a622bf0a65f9549ff5c5658562ae26
conconrob <connortprobertson@gmail.com>
2019-04-16 09:48:02 +0100

    Done restrict admin mw

--- api/authorization/authenticate.js
+++ api/authorization/authenticate.js
@@ -3,7 +3,7 @@ const jwtSecret = process.env.JWT_SECRET || "mellon";
 
 const db = require("../../data/dbConfig.js");
 
-module.exports = { restrict, generateToken };
+module.exports = { restrict, generateToken, restrictAdmin };
 
 function restrict(req, res, next) {
   const token = req.header("Authorization");
@@ -29,6 +29,45 @@ function restrict(req, res, next) {
     });
   } else {
     res.status(403).json({ message: "No valid credentials provided" });
+  }
+}
+
+function restrictAdmin(req, res, next) {
+  if (req.header("Authorization")) {
+    if (!req.user) {
+      // needs basic auth
+      const token = req.header("Authorization");
+      jwt.verify(token, jwtSecret, (error, decocedToken) => {
+        if (error) {
+          res.status(403).json({ message: "No valid credentials provided" });
+        } else {
+          const id = decocedToken.subject;
+          db("users")
+            .where({ id })
+            .first()
+            .then(user => {
+              req.user = user;
+            })
+            .catch(error => {
+              res
+                .status(403)
+                .json({ message: "No valid credentials provided" });
+            });
+        }
+      });
+    }
+    if (req.user) {
+      // need to check if admin
+      if (req.user.user_type_id === 1) {
+        next();
+      } else {
+        res.status(401).json({
+          message: "User does not have permission to perform this action."
+        });
+      }
+    }
+  } else {
+    res.status(403).json({ message: "No valid credentials provided", error });
   }
 }
 
