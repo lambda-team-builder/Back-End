a07b0b99f7006451579c4866f2312dc7d397ea80
conconrob <connortprobertson@gmail.com>
2019-04-17 09:10:14 +0100

    list of user's admins docs done

--- api/routers/classroom_admins-router.js
+++ api/routers/classroom_admins-router.js
@@ -2,6 +2,46 @@ const router = require("express").Router();
 
 const ClassroomAdmin = require("../../data/models/classroomAdminsModel");
 
+/**
+ *  @api {get} api/classroom_admins/ Get a list of user's classrooms that user admin's
+ *  @apiVersion 0.1.0
+ *  @apiName getClassroomAdmins
+ *  @apiGroup ClassroomAdmins
+ *
+ *  @apiHeader {String} Authorization Users auth token.
+ *
+ *  @apiSuccess {Array} projects A list of the classroom's projects
+ *
+ *  @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *
+ *    [
+ *       {
+ *           "classroom_admins_id": 1,
+ *           "classroom_id": 1,
+ *           "classroom_name": "build week 1"
+ *       },
+ *       {
+ *           "classroom_admins_id": 2,
+ *           "classroom_id": 2,
+ *           "classroom_name": "build week 2"
+ *       },
+ *       {
+ *           "classroom_admins_id": 3,
+ *           "classroom_id": 3,
+ *           "classroom_name": "build week 3"
+ *       }
+ *    ]
+ *
+ *
+ *  @apiErrorExample Error-Response:
+ *    HTTP/1.1 500 INTERNAL SERVER ERROR
+ *    {
+ *      "message": "server Error",
+ *      "error": {error object: ""}
+ *    }
+ */
+
 router.get("/", (req, res) => {
   const user_id = req.user.id;
   ClassroomAdmin.getclassroomAdminsByUserId(user_id)
--- docs/api_project.js
+++ docs/api_project.js
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-16T18:29:56.928Z",
+    "time": "2019-04-17T08:09:41.362Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
